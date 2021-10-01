d735012a7791b304dd4233c37847ee666e4470fd
Archangel2903 <archangel2903@gmail.com>
2021-10-01 17:43:19 +0300

    main page stages

--- src/js/index.js
+++ src/js/index.js
@@ -41,12 +41,15 @@ $(function () {
 
 // Swiper slider
     (function() {
-        if ($('.swiper-container').length) {
+        let defaultSlider = $('.swiper');
+        let stagesSliderWrap = $('.stages-slider-wrapper');
+
+        if (defaultSlider.length) {
             let slider;
             let slide = document.querySelectorAll('.swiper-container .swiper-slide').length;
 
             if (slide > 1) {
-                slider = new Swiper('.swiper-container', {
+                slider = new Swiper('.swiper', {
                     observer: true,
                     observeParents: true,
                     loop: true,
@@ -59,11 +62,71 @@ $(function () {
                     },
                     pagination: {
                         el: '.swiper-pagination',
-                        clickable: true
+                        type: "fraction",
                     },
                     dynamicBullets: true,
                 });
             }
+        }
+
+        if (stagesSliderWrap.length) {
+            let stagesSliderNav = document.querySelector('.stages-slider-nav');
+            let stagesSliderText = document.querySelector('.stages-slider-content');
+
+            let contentSlider = new Swiper('.stages-slider-content', {
+                // observer: true,
+                // observeParents: true,
+                loop: true,
+                slidesPerView: 1,
+            });
+
+            let mainSlider = new Swiper('.stages-slider-nav', {
+                // observer: true,
+                // observeParents: true,
+                loop: true,
+                slidesPerView: 1,
+                watchSlidesProgress: true,
+                navigation: {
+                    prevEl: stagesSliderNav.querySelector('.swiper-button-prev'),
+                    nextEl: stagesSliderNav.querySelector('.swiper-button-next'),
+                },
+                pagination: {
+                    el: ".swiper-pagination",
+                    type: "fraction",
+                },
+                thumb: {
+                    swiper: contentSlider,
+                },
+            });
+
+            /*let stagesSliderNav = document.querySelector('.stages-slider-nav');
+            let stagesSliderText = document.querySelector('.stages-slider');
+
+            let sliderNav = new Swiper('.stages-slider-nav', {
+                loop: true,
+                slidesPerView: 1,
+                watchSlidesProgress: true,
+                navigation: {
+                    nextEl: stagesSliderNav.querySelector('.swiper-button-next'),
+                    prevEl: stagesSliderNav.querySelector('.swiper-button-prev'),
+                },
+                pagination: {
+                    el: stagesSliderNav.querySelector('.swiper-pagination'),
+                    type: "fraction",
+                },
+            });
+
+            let sliderContent = new Swiper('.stages-slider', {
+                loop: true,
+                slidesPerView: 1,
+                allowTouchMove: false,
+                thumbs: {
+                    swiper: sliderNav,
+                },
+            });
+
+            console.log(sliderNav);
+            console.log(sliderContent);*/
         }
     })();
 
